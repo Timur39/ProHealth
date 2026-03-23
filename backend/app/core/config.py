6e6328a971a73a6a485483a50147d0bbb91678bb
Timur Shiplivcev <timur430808@gmail.com>
2026-03-23 19:42:16 +0300

    Add alembic and poetry

---
+++ backend/app/core/config.py
@@ -0,0 +1,14 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    AUTH_SECRET_KEY: str
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+
+    DATABASE_URL: str
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+
+settings = Settings()
