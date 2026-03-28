80b6f84de8ec8821f6dc35f4c5178c20ace06721
Timur Shiplivcev <timur430808@gmail.com>
2026-03-28 21:34:20 +0300

    Add env data to security.py 2

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -17,4 +17,4 @@ def create_access_token(data: dict):
     expire = datetime.utcnow() + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
 
     to_encode.update({"exp": expire})
-    return jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+    return jwt.encode(to_encode, settings.AUTH_SECRET_KEY, algorithm=settings.ALGORITHM)
